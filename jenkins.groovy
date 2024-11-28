e4e62639adaff7a9ce8ec1f60ddb2c2213fe1115
Dzmitry Karpuk <dzmitry.karpuk@corp.itibo.com>
2024-11-28 14:46:05 +0300

    jhdgaudfyjghg

--- jenkins.groovy
+++ jenkins.groovy
@@ -20,7 +20,7 @@ node {
         }
 
         try {
-            parallel getTestStages(["ui", "uiT"])
+            parallel getTestStages(["test"])
         } finally {
             stage ("Allure") {
                 generateAllure()
@@ -28,19 +28,6 @@ node {
         }
     }
 }
-
-
-def getTestStages(testTags) {
-    def stages = [:]
-    testTags.each { tag ->
-        stages["${tag}"] = {
-            runTestWithTag(tag)
-        }
-    }
-    return stages
-}
-
-
 
 def runTestWithTag() {
         labelledShell(label: "Run", script: "test")
