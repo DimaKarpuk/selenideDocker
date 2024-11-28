3642226e7b1fb8ecd0de2fe82148ec59c19ee57a
Dzmitry Karpuk <dzmitry.karpuk@corp.itibo.com>
2024-11-28 14:36:24 +0300

    hhuuuuuuuu

--- src/test/java/Tests.java
+++ src/test/java/Tests.java
@@ -2,6 +2,7 @@ import com.codeborne.selenide.Configuration;
 import org.junit.jupiter.api.Tag;
 import org.junit.jupiter.api.Test;
 
+import static com.codeborne.selenide.Condition.text;
 import static com.codeborne.selenide.Selenide.$;
 import static com.codeborne.selenide.Selenide.open;
 import static io.qameta.allure.Allure.step;
@@ -24,7 +25,7 @@ public class Tests extends TestBase {
         Configuration.headless=true;
         open("https://oz.by/");
         step("ref cookie",()->{
-            $("[data-action='click->cookie-dialog#handleRejectClick']").click();
+            $("[data-action='click->cookie-dialog#handleRejectClick']").shouldHave(text("iiiiiii"));
         });
     }
 }
