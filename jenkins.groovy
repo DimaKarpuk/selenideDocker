ea4d07cffd1084b76a09e041fd1bf8bd236faeba
Dzmitry Karpuk <dzmitry.karpuk@corp.itibo.com>
2024-11-28 13:36:31 +0300

    updated tt

--- jenkins.groovy
+++ jenkins.groovy
@@ -18,16 +18,49 @@ node {
                 echo "Current branch is master"
             }
         }
+
+        try {
+            parallel getTestStages(["apiTests", "uiTests"])
+        } finally {
             stage ("Allure") {
                 generateAllure()
+            }
         }
+
+//        try {
+//            stage("Run tests") {
+//                parallel(
+//                        'Api Tests': {
+//                            runTestWithTag("apiTests")
+//                        },
+//                        'Ui Tests': {
+//                            runTestWithTag("uiTests")
+//                        }
+//                )
+//            }
+//        } finally {
+//            stage("Allure") {
+//                generateAllure()
+//            }
+//        }
     }
 }
 
 
-def runTestWithTag() {
+def getTestStages(testTags) {
+    def stages = [:]
+    testTags.each { tag ->
+        stages["${tag}"] = {
+            runTestWithTag(tag)
+        }
+    }
+    return stages
+}
+
+
+def runTestWithTag(String tag) {
     try {
-        labelledShell(label: "Run", script: "chmod +x gradlew \n./gradlew -x test")
+        labelledShell(label: "Run ${tag}", script: "chmod +x gradlew \n./gradlew -x test ${tag}")
     } finally {
         echo "some failed tests"
     }
@@ -49,9 +82,13 @@ def generateAllure() {
             jdk              : '',
             properties       : [],
             reportBuildPolicy: 'ALWAYS',
-            results          : [[path: 'build/allure-result']]
+            results          : [[path: 'build/allure-results']]
     ])
 }
+
+
+
+
 
 
 
